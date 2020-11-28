f5713dd2049d928ec40d38c0872fe1562633d640
francisca abbey blay <fablay002@st.ug.edu.gh>
2020-11-28 13:54:24 +0000

    second changes

--- weatherapp/src/pages/Home.jsx
+++ weatherapp/src/pages/Home.jsx
@@ -8,38 +8,40 @@ function Home (){
     const [coords, setCoords] = useState({})
     const [weatherData, setWeatherData] = useState({})
 
-    const getCurrentWeather = (coords) => {
-        if (coords.longitude !==undefined){
+    const getCurrentWeather = () => {
+        if (coords.longitude !==undefined) {
         axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${coords.latitude}&lon=${coords.longitude}&appid=${APP_ID}`)
-        .then((response)=>{
+        .then((response)=> {
             setWeatherData(response.data)
             console.log(response.data)
         })
     } 
+}
 useEffect(()=>{
     navigator.geolocation.getCurrentPosition( (position)=> {
         setCoords(position.coords);
         getCurrentWeather()
         
-    }, (error) =>{
+    }, (error)=> {
         console.log("hey this is your err:", error)
     })
     
-})
+}, [])
 
     return(
-<div className='container'>
-    <div className="content"></div>
+<div className="container">
+    <div className="content">
         <div className="left">
             <h3>Fri, 27<br/>November</h3>
-            <div className='currentWeatherInfo'>
-                <p className='temp'>10&deg;</p>
+            </div>
+            <div className="right">sdfjk</div>
+                
             </div>
         </div>
-        <div className='right'>sdjfk</div>
-    .
-</div>
+        
+    
+
     )
 }
-}
+
 export default Home;
