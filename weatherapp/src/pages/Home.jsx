f725ab097b8f94a3366a6c54bc4b6d4c7a5577b4
francisca abbey blay <fablay002@st.ug.edu.gh>
2020-11-29 16:54:39 +0000

    sunday changes

--- weatherapp/src/pages/Home.jsx
+++ weatherapp/src/pages/Home.jsx
@@ -1,10 +1,20 @@
 import React, {useEffect, useState} from 'react';
 import axios from 'axios';
 
+import '../styles/home.css'
+import weatherIcon1 from '../images/weather-icon1.png'
+import weatherIcon2 from '../images/weather-icon2.png'
+import weatherIcon3 from '../images/weather-icon3.png'
+import weatherIcon4 from '../images/weather-icon4.png'
+import weatherIcon5 from '../images/weather-icon5.png'
+import weatherIcon6 from '../images/weather-icon6.png'
+
+
+
 
 
 const APP_ID = "6ecad96bc440cb7b02fb1cc8115ae284"
-function Home (){
+function Home () {
     const [coords, setCoords] = useState({})
     const [weatherData, setWeatherData] = useState({})
 
@@ -33,6 +43,42 @@ useEffect(()=>{
     <div className="content">
         <div className="left">
             <h3>Fri, 27<br/>November</h3>
+            <div className="currentWeatherInfo">
+            <p className="temp">-10&deg;</p>
+                        <img src={weatherIcon1} alt="Weather Icon"/>
+                        {/* <p className="description">Cloudy with a little rainy drops</p> */}
+                        </div>
+                        <div className="weekWeatherInfo">
+                        <div className="weather-info">
+                            <h5>MON</h5>
+                            <img src={weatherIcon2} alt="Weather Info 2" />
+                            <p>12&deg;</p>
+                        </div>
+                        <div className="weather-info">
+                            <h5>TUE</h5>
+                            <img src={weatherIcon3} alt="Weather Info 3" />
+                            <p>12&deg;</p>
+                        </div>
+                        <div className="weather-info">
+                            <h5>WED</h5>
+                            <img src={weatherIcon4} alt="Weather Info 4" />
+                            <p>12&deg;</p>
+                        </div>
+                        <div className="weather-info">
+                            <h5>THUR</h5>
+                            <img src={weatherIcon5} alt="Weather Info 5" />
+                            <p>12&deg;</p>
+                        </div>
+                        <div className="weather-info">
+                            <h5>FRI</h5>
+                            <img src={weatherIcon6} alt="Weather Info 6" />
+                            <p>12&deg;</p>
+                        </div>
+
+
+
+
+</div>
             </div>
             <div className="right">sdfjk</div>
                 
--- weatherapp/src/pages/Login.jsx
+++ weatherapp/src/pages/Login.jsx
@@ -1,10 +1,42 @@
-import React from 'react'
+import React, { useState } from "react";
+import "./styles/Auth.css";
 
-function Login (){
-    return(
-<div>
-    <h1>this is my login page</h1>
-</div>
-    )
+function Login() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  function handleUsernameOnChange(event) {
+    setUsername(event.target.value);
+  }
+  function handlePasswordOnChange(event) {
+    setPassword(event.taret.value);
+  }
+
+  function loginUser(event) {
+    if (username === password) {
+      alert("Welcome");
+    } else {
+      alert("Invalid Credentials");
+    }
+  }
+
+  return (
+    <div className={"auth-container"}>
+      <form className={"auth"} onSubmit={loginUser}>
+        <input
+          onChange={handleUsernameOnChange}
+          value={"Username"}
+          type={"text"}
+          placeholder={"Please enter your username"}
+        />
+        <input
+          onChange={handlePasswordOnChange}
+          value={"password"}
+          placeholder={"Please enter your password"}
+        />
+        <button type={"submit"}>Login</button>
+      </form>
+    </div>
+  );
 }
 export default Login;
