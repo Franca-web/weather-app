996257d336d0ca0d763037ab7a9f31fb4f7f58c7
francisca abbey blay <fablay002@st.ug.edu.gh>
2020-12-02 16:15:17 +0000

    appcontext changes

--- weatherapp/src/App.js
+++ weatherapp/src/App.js
@@ -2,23 +2,35 @@ import {Switch,Route} from 'react-router-dom'
 import Login from './pages/Login'
 import SignUp from './pages/SignUp'
 import Home from './pages/Home'
-import SearchWeather from './pages/SearchWeather'
-  
+import SearchWeather from './pages/SearchWeather';
+import './styles/variables.css';
+import React, {useState} from 'react';
+
 
 
 import './App.css';
 
+export const AppContext = React.createContext()
+
+
 function App() {
+
+  const [isLoading, setIsLoading] = useState(false);
+
+
   return (
+<AppContext.Provider value={{isLoading, setIsLoading}}>
+
     <div className="App">
       <Switch>
         <Route path={"/"} exact={true} component={Home} />
         <Route path={"/login"} exact={true} component={Login} />
         <Route path={"/signup"} exact={true} component={SignUp} />
-        <Route path={"/search"} exact={true} components={SearchWeather} />
+        <Route path={"/search"} exact={true} component={SearchWeather} />
       </Switch>
       
     </div>
+     </AppContext.Provider>
   );
 }
 
--- weatherapp/src/pages/SearchWeather.jsx
+++ weatherapp/src/pages/SearchWeather.jsx
@@ -1,32 +1,39 @@
+import axios from 'axios';
 import React, {useState} from 'react';
 
-const APP_URL = "6ecad96bc440cb7b02fb1cc8115ae284"
+const APP_ID = "6ecad96bc440cb7b02fb1cc8115ae284"
 
 function SearchWeather(){
-    const [searchWeather, setSearchWeather] = useState("") 
+    const [searchQuery, setSearchQuery] = useState("") 
     const search = (e) => {
-        
-        const APP_URL = `https://api.openweathermap.org/data/2.5/weather?q=${searchWeather}&appid=${APP_URL}`
         e.preventDefault()
-        console.log(searchWeather)
-    }
+
+        axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${searchQuery}&appid=${APP_ID}`)
+        .then((search)=> {
+            setSearchQuery(search.query)
+       
+        console.log(search.query)
+    })
+}
     return(
         <div className="container">
             <div className="content">
                 <div className="left">
-                    <form>
-                        <input value ={searchWeather} onChange={(e)=>setSearchWeather(e.target.value)} placeholder="enter location to set" type ="text"/>
-<button type ="submit">Search</button>
+                    <form onSubmit ={search}>
+                        <input value ={searchQuery} onChange={(e)=>setSearchQuery(e.target.value)} placeholder="enter location to search" type ="text"/>
+                        <button type ="submit">Search</button>
                         
                     </form>
                 </div>
-            </div>
-        <h1>
-            hey search weather
-        </h1>
+            <div className="right">
+        </div>
+            
+        
 
+        </div>
         </div>
 
     )
 }
+
 export default SearchWeather;
